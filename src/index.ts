43bf24d9b0b9a170e0ab8db7e978c1bb95189405
ssekridov@gmail.com <ssekridov@gmail.com>
2023-07-03 13:59:59 +0200

    feat: server run and listen to port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import process from 'process';
+import { createServer } from 'node:http';
 
 const port = process.env.PORT;
 
-console.log('Kekus!', `port = ${port}`);
+const app = createServer((_, res) => {
+  res.end('Request accepted');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
